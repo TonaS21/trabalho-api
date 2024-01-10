fb75657c1b1846b24d33683c8aa27ac1dfd9a49f
António Barros <abotinasbarros@gmail.com>
2024-01-10 23:00:58 +0000

    m

--- Script/Player/tiro_attack.cs
+++ Script/Player/tiro_attack.cs
@@ -5,7 +5,7 @@ public partial class tiro_attack : Area2D
 {
 	//public int level = 1; Para outra altura.
 	public int hp = 1;
-	public int speed;
+	public int vel;
 	public int damage;
 	//public int knock_amount = 100;
 	public int attack_size = 1;
@@ -13,31 +13,28 @@ public partial class tiro_attack : Area2D
 	public Vector2 mousePos;
 	public Vector2 angle = Vector2.Zero;
 	public Vector2 target = Vector2.Zero;
+	public Vector2 pos;
+	public Vector2 direction;
 
-	public PackedScene SwordSlash = GD.Load<PackedScene>("res://tiro_attack.tscn");
-
-	public CharacterBody2D Player; 
+	public PackedScene SwordSlash = GD.Load<PackedScene>("res://tiro_attack.tscn"); 
 	
 	public override void _Ready()
 	{
-		mousePos = GetGlobalMousePosition();
-		speed = 150;
+		vel = 1;
 		damage = 100;
-		angle = GlobalPosition.DirectionTo(target);
-		Rotation = angle.Angle() + Mathf.DegToRad(135);
+		//mousePos = GetGlobalMousePosition();
+		var tiro = SwordSlash.Instantiate<Area2D>();
+		pos = GetNode<CharacterBody2D>("Player").Position;
+		//Position = pos + pos.DirectionTo(mousePos) * 40;
+		AddSibling(tiro);
 	}
 
 	public override void _Process(double delta)
 	{
-		float deltaFloat = (float)delta;
-		Position += angle*speed*deltaFloat;
-	}
-
-	public void enemy_hit(int charge = 1) {
-		hp -= charge;
-		if (hp <= 0) {
-			QueueFree();
-		}
+		mousePos = GetGlobalMousePosition();
+		Position = pos + pos.DirectionTo(mousePos) * 40;
+		direction = Position.DirectionTo(mousePos);
+		Position += direction*vel;
 	}
 	
 	private void _on_timer_timeout()
